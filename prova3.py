74a9793b81e1a3a24de02722c64f011ba51c2b2b
João Igor <igor.joao@academico.ifpb.edu.br>
2025-03-13 21:49:49 -0300

    refactor: corrigir implementação do código fornecido na prova 3

--- prova3.py
+++ prova3.py
@@ -1,16 +1,19 @@
+def calc_pontos(valor, redex, cartaox):
+    if not isinstance(valor, (int, float)):
+        raise ValueError("O valor da compra deve ser um número.")
+    
+    if valor < 0:
+        raise ValueError("O valor da compra não pode ser negativo.")
+    
+    valor = int(valor)  # descarta os centavos 
+    
+    if redex and cartaox:
+        pontos = valor * 3
+    elif redex:
+        pontos = valor * 1.5
+    elif cartaox:
+        pontos = valor
+    else:
+        pontos = 0
 
-# código fornecido na prova
-
-def calcPontos(valorCompra, redex, cartaox):
-  pontos = 0
-  
-  if redex and cartaox:
-    pontos = valor * 3
-  
-  elif redex:
-    pontos = valor * 1.5
-  
-  elif cartaox:
-    pontos = valor
-  
-  return pontos
+    return int(pontos)  # retorna um inteiro
